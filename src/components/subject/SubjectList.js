fc2530935e48b6c95b6af990a5460d9e0b35a4b7
Arlen Neylon <aneylon@scramsystems.com>
2022-09-22 10:12:24 -0600

    setup for subject sidebar.

---
+++ src/components/subject/SubjectList.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const SubjectList = () => {
+  const [subjects, setSubjects] = useState();
+  function GetSubjects() {
+    console.log("getting subjects");
+    fetch("http://localhost:3737/subjects")
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setSubjects(data);
+      })
+      .catch((error) => console.error("Error fetching subjects : ", error));
+  }
+  useEffect(() => {
+    GetSubjects();
+  }, []);
+  return (
+    <div>
+      {subjects.length && (
+        <ul>
+          {subjects.map((subject) => {
+            return (
+              <li key={subject.id}>
+                <span title={subject.subTitle}>{subject.title}</span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+export default SubjectList;
