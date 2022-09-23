8f655da8152af39a4fabdcf3a0ec71614e3af672
Arlen Neylon <aneylon@scramsystems.com>
2022-09-23 17:27:45 -0600

    Add cards.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import "./App.css";
-import AddLesson from "./components/lesson/AddLesson";
-import LessonList from "./components/lesson/LessonList";
-import AddSubject from "./components/subject/AddSubject";
-import SubjectList from "./components/subject/SubjectList";
-import Todo from "./components/todo/todo";
+import FlashCards from "./components/card/FlashCards";
 
 function App() {
   return (
     <div className="App">
-      <AddLesson />
-      <LessonList subjectId={2} />
-      {/*
-      <SubjectList />
-      <AddSubject />
-      <Todo />
-      */}
+      <FlashCards id={1} />
     </div>
   );
 }
