409d42bf54ee7bc84eb59ec7047041f8d27dce1b
Arlen Neylon <aneylon@scramsystems.com>
2022-09-27 07:57:33 -0600

    Add card list.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import AddCard from "./components/card/AddCard";
+import CardList from "./components/card/CardList";
 
 function App() {
   return (
     <div className="App">
       <AddCard />
+      <CardList />
     </div>
   );
 }
