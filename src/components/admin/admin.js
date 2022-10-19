15fefe793ad892e4f9be0f73b7e65125897e0185
Arlen Neylon <aneylon@scramsystems.com>
2022-10-19 12:53:28 -0600

    Set up admin component and protected route.

---
+++ src/components/admin/admin.js
@@ -0,0 +1,10 @@
+import { Box, Toolbar, Typography } from "@mui/material";
+const Admin = () => {
+  return (
+    <Box component="main" sx={{ flexGrow: 1, p: 5, marginLeft: 15 }}>
+      <Toolbar />
+      <Typography>Admin</Typography>
+    </Box>
+  );
+};
+export default Admin;
