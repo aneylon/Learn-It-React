3ce4d0edc40c317393d99f40384353973ff0889d
Arlen Neylon <aneylon@scramsystems.com>
2022-10-28 11:19:36 -0600

    Set up lesson list.

--- src/components/lesson/LessonList.js
+++ src/components/lesson/LessonList.js
@@ -1,38 +1,84 @@
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { useEffect, useState } from "react";
+import { request } from "../../api/apiUtilities";
+
 let apiUrl = process.env.REACT_APP_API;
 const LessonList = ({ subjectId }) => {
   const [lessons, setLessons] = useState([]);
-  function GetLessonsFor(subjectId) {
-    fetch(`${apiUrl}/lessons?subjectId=${subjectId}`)
+  const GetAllLessons = () => {
+    request("get", `${apiUrl}/lesson`, {})
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        setLessons(data);
+        setLessons(data.lessons);
       })
-      .catch((error) => console.error(error));
-  }
-  const GetAllLessons = () => {
-    console.log("get all lessons");
+      .catch(console.error);
+  };
+  const EditItem = (id) => {
+    console.log("edit ", id);
+  };
+  const DeleteItem = (id) => {
+    console.log("delete  ", id);
   };
   useEffect(() => {
-    // GetLessonsFor(subjectId);
     GetAllLessons();
   }, []);
   return (
     <div>
-      Lesson List : {subjectId}
-      {lessons.length > 0 && (
-        <ul>
-          {lessons.map((lesson) => {
-            return (
-              <li key={lesson.id}>
-                <span title={lesson.subTitle}>{lesson.title}</span>
-              </li>
-            );
-          })}
-        </ul>
-      )}
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell>SubTitle</TableCell>
+              <TableCell>Subject</TableCell>
+              <TableCell align="center">Edit</TableCell>
+              <TableCell align="right">Delete</TableCell>
+            </TableRow>
+          </TableHead>
+          {lessons.length > 0 && (
+            <TableBody>
+              {lessons.map((lesson) => {
+                return (
+                  <TableRow key={lesson._id}>
+                    <TableCell>{lesson.title}</TableCell>
+                    <TableCell>{lesson.subTitle}</TableCell>
+                    <TableCell>{lesson.subjectId}</TableCell>
+                    <TableCell align="center">
+                      <Button
+                        onClick={() => {
+                          EditItem(lesson._id);
+                        }}
+                      >
+                        Edit
+                      </Button>
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button
+                        onClick={() => {
+                          DeleteItem(lesson._id);
+                        }}
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          )}
+        </Table>
+      </TableContainer>
     </div>
   );
 };
