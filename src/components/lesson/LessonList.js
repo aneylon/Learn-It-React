f1916ce47dda6b9f734bb63b749151bf8d4dc848
Arlen Neylon <aneylon@scramsystems.com>
2022-10-28 12:26:50 -0600

    Set up delete modal.

--- src/components/lesson/LessonList.js
+++ src/components/lesson/LessonList.js
@@ -7,13 +7,44 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
 } from "@mui/material";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import { request } from "../../api/apiUtilities";
 
 let apiUrl = process.env.REACT_APP_API;
 const LessonList = ({ subjectId }) => {
   const [lessons, setLessons] = useState([]);
+  const [itemToDelete, setItemToDelete] = useState(null);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [showEditDialog, setShowEditDialog] = useState(false);
+  const ConfirmDelete = () => {
+    request("delete", `${apiUrl}/lesson/${itemToDelete}`, {})
+      .then((response) => {
+        if (response.ok === true && response.status === 200) {
+          toast.success("Deleted item");
+        } else {
+          toast.error("Unable to delete item");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // do something with data?
+        GetAllLessons();
+        setItemToDelete(null);
+      })
+      .catch(console.error);
+    setShowDeleteDialog(false);
+  };
+  const CancelDelete = () => {
+    setShowDeleteDialog(false);
+    setItemToDelete(null);
+  };
   const GetAllLessons = () => {
     request("get", `${apiUrl}/lesson`, {})
       .then((response) => {
@@ -28,7 +59,8 @@ const LessonList = ({ subjectId }) => {
     console.log("edit ", id);
   };
   const DeleteItem = (id) => {
-    console.log("delete  ", id);
+    setItemToDelete(id);
+    setShowDeleteDialog(true);
   };
   useEffect(() => {
     GetAllLessons();
@@ -79,6 +111,18 @@ const LessonList = ({ subjectId }) => {
           )}
         </Table>
       </TableContainer>
+      <Dialog open={showDeleteDialog} onClose={CancelDelete}>
+        <DialogTitle>Delete Item?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you to delete this item?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={CancelDelete}>Cancel</Button>
+          <Button onClick={ConfirmDelete}>Ok</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
